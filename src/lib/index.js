f1b452f2746e3b07cc01ab5320156b9ad1c48e1d
Lbaruch <aviadam.segel@gmail.com>
2025-08-28 22:31:26 +0300

    fix css sr-only

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,1 @@
-// Export all your components
 export { default as ShareButtons } from './share/shareButtons/index.svelte';
-// Export any utilities or types
-
-// Export styles
-import './styles.css';
